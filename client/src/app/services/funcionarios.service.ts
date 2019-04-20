a944c7fd596d55bbe35d66baa5a161d3fd235a63
Kimikasumi <lcamargos@unbosque.edu.co>
2019-04-20 18:11:32 -0500

    Avance app-routing

--- client/src/app/services/funcionarios.service.ts
+++ client/src/app/services/funcionarios.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionariosService {
+  API_URI = 'http://localhost:3000/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFuncionarios(){
+    return this.http.get(`${this.API_URI}/workers`);
+  }
+
+  getFuncionario(cod_funcionario: string){
+    return this.http.get(`${this.API_URI}/workers/${cod_funcionario}`)
+  }
+
 }
