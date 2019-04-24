93682e507a95259ce073fdf83ed78f207184820a
nokchoa <camilonovoam@gmail.com>
2019-04-24 13:49:32 -0500

    fortmating

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,39 +9,43 @@ import { PrincipalProduccionComponent } from './components/Produccion/principal-
 import { ListarMPComponent } from './components/Produccion/MateriaPrima/listar-mp/listar-mp.component';
 import { AgregarMPComponent } from './components/Produccion/MateriaPrima/agregar-mp/agregar-mp.component';
 import { PrincipalMPComponent } from './components/Produccion/principal-mp/principal-mp.component';
-import { FormFuncionarioComponent} from './components/RRHH/form-funcionario/form-funcionario.component'
-import {PrincipalRrhhComponent} from './components/RRHH/principal-rrhh/principal-rrhh.component'
+import { FormFuncionarioComponent } from './components/RRHH/form-funcionario/form-funcionario.component'
+import { PrincipalRrhhComponent } from './components/RRHH/principal-rrhh/principal-rrhh.component'
 import { DashboardRrhhComponent } from './components/RRHH/dashboard-rrhh/dashboard-rrhh.component';
 import { LoginComponent } from './components/login/login.component';
 const routes: Routes = [
-{
-  path: '',
-  redirectTo: '/login',
-  pathMatch: 'full'
-},
-{
-  path: 'login',
-  component: LoginComponent
-},
-{path: 'workers',
-children:[
-  {path: 'agregar', component: FormFuncionarioComponent},
-  {path: 'listado', component: PrincipalRrhhComponent},
-  {path: 'dashboard', component: DashboardRrhhComponent},
-  {path: 'editar/:cod_funcionario', component: FormFuncionarioComponent}
-]},
-{path: 'produccion', 
-children:[
-  {path: 'listar', component: ListarComponent},
-  {path: 'agregar', component: AgregarComponent},
-  {path: 'principal', component: PrincipalProduccionComponent},
-  {path: 'editar/:id', component: AgregarComponent},
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'workers',
+    children: [
+      { path: 'agregar', component: FormFuncionarioComponent },
+      { path: 'listado', component: PrincipalRrhhComponent },
+      { path: 'dashboard', component: DashboardRrhhComponent },
+      { path: 'editar/:cod_funcionario', component: FormFuncionarioComponent }
+    ]
+  },
+  {
+    path: 'produccion',
+    children: [
+      { path: 'listar', component: ListarComponent },
+      { path: 'agregar', component: AgregarComponent },
+      { path: 'principal', component: PrincipalProduccionComponent },
+      { path: 'editar/:id', component: AgregarComponent },
 
-  {path: 'listarMP', component: ListarMPComponent},
-  {path: 'agregarMP', component: AgregarMPComponent},
-  {path: 'principalMP', component: PrincipalMPComponent},
-  {path: 'editarMP/:id', component: AgregarMPComponent}
-]},
+      { path: 'listarMP', component: ListarMPComponent },
+      { path: 'agregarMP', component: AgregarMPComponent },
+      { path: 'principalMP', component: PrincipalMPComponent },
+      { path: 'editarMP/:id', component: AgregarMPComponent }
+    ]
+  },
 
 ];
 
