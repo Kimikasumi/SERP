bd9dc96d76ed589250390e8d5030cac295a41be6
nokchoa <camilonovoam@gmail.com>
2019-04-24 21:18:09 -0500

    Componentes nuevos del area de PV

---
+++ client/src/app/components/PV/listar-pv/listar-pv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pv',
+  templateUrl: './listar-pv.component.html',
+  styleUrls: ['./listar-pv.component.css']
+})
+export class ListarPvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ client/src/app/components/PV/navbar-pv/navbar-pv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-pv',
+  templateUrl: './navbar-pv.component.html',
+  styleUrls: ['./navbar-pv.component.css']
+})
+export class NavbarPvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
