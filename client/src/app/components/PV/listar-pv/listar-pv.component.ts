eba2194ac51eb766819ab315b6238d0d8c6de0d5
nokchoa <camilonovoam@gmail.com>
2019-04-24 23:16:25 -0500

    funcionamiento listado de datos

--- client/src/app/components/PV/listar-pv/listar-pv.component.ts
+++ client/src/app/components/PV/listar-pv/listar-pv.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RetailersService } from 'src/app/services/retailers.service';
 
 @Component({
   selector: 'app-listar-pv',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPvComponent implements OnInit {
 
-  constructor() { }
+  sucurs: any = []
+  constructor(private retailerService: RetailersService) { }
 
   ngOnInit() {
+    this.mostrarSucursales();
   }
 
+  mostrarSucursales() {
+    this.retailerService.getSucursales().subscribe(
+      res => {
+        console.log(res)
+        this.sucurs = res;
+      },
+      err => console.log(err)
+    )
+  }
+
+  eliminarPV(cod_retail: string) {
+    console.log(cod_retail);
+    this.retailerService.deleteRetail(cod_retail).subscribe(
+      res => {
+        console.log("se elimino " + cod_retail);
+        console.log(res);
+        this.mostrarSucursales();
+      },
+      err => console.log(err)
+    )
+  }
 }
