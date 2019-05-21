fd59a6b436de6de46082bcf4995393c5389f85f2
nokchoa <camilonovoam@gmail.com>
2019-05-20 21:42:59 -0500

    Agrego componente de listado (sin terminar)

---
+++ client/src/app/components/PV/listar-inv-pv/listar-inv-pv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-inv-pv',
+  templateUrl: './listar-inv-pv.component.html',
+  styleUrls: ['./listar-inv-pv.component.css']
+})
+export class ListarInvPvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
